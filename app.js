4780dc62c68a6fc474326d8d29f904d2fdcf3fa0
TUNGNT <nguyenthanhtunnghutechsg@gmail.com>
2026-04-01 15:01:52 +0700

    CRUD message

--- app.js
+++ app.js
@@ -29,6 +29,7 @@ app.use('/api/v1/categories', require('./routes/categories'))
 app.use('/api/v1/auth', require('./routes/auth'))
 app.use('/api/v1/carts', require('./routes/cart'))
 app.use('/api/v1/upload', require('./routes/upload'))
+app.use('/api/v1/messages', require('./routes/messages'))
 
 mongoose.connect('mongodb://localhost:27017/NNPTUD-C4');
 mongoose.connection.on('connected', function () {
